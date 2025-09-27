bda44103b2122e56623f2bde65fcccf8be337c63
unknown <nizdvfc@web.de>
2025-09-27 20:58:31 +0200

    FIX: player doesnt go backwards

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,19 +15,19 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_W):
 		#if !(depth <= max_depth):
-		speed.y = move_toward(speed.y, max_speed, 0.05)
+		speed.y = move_toward(speed.y, -max_speed, 0.05)
 		
 	elif Input.is_key_pressed(KEY_S):
 		#if !(depth <= max_depth):
-		speed.y = move_toward(speed.y, -max_speed, 0.05)
+		speed.y = move_toward(speed.y, max_speed, 0.05)
 		
 	if not (Input.is_key_pressed(KEY_W) or Input.is_key_pressed(KEY_S)):
 		speed.y = move_toward(speed.y, 0.0, 0.05)
 		# if motionless add drift
 		
 	if Input.is_key_pressed(KEY_A):
-		speed.x = move_toward(speed.x, max_speed, 0.05)
+		speed.x = move_toward(speed.x, -max_speed, 0.05)
 		
 	elif Input.is_key_pressed(KEY_D):
-		speed.x = move_toward(speed.x, -max_speed, 0.05)
+		speed.x = move_toward(speed.x, max_speed, 0.05)
 	move_and_collide(speed)
