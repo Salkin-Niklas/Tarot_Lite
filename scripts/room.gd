48bbd2d8b1f049688a1647f0ba0604e64ce38743
MasterOktagon <userkontakt.hexagongames@gmail.com>
2025-09-27 20:39:09 +0200

    ADD: example for room generation

--- scripts/room.gd
+++ scripts/room.gd
@@ -4,10 +4,9 @@ extends Node2D
 
 var size: Vector2i
 
-func build(t: TileMapLayer, ground: TileMapLayer) -> void:
+func build(t: TileMapLayer) -> void:
 	var cells: Array[Vector2i] = []
 	for x in range(size.x):
 		for y in range(size.y):
-			cells.append(Vector2i(position) + Vector2i(x,y))
-			ground.set_cell(Vector2i(x,y), 0, Vector2i(6,2))
+			cells.append(Vector2i(position/16) + Vector2i(x,y))
 	t.set_cells_terrain_connect(cells,0,0)
